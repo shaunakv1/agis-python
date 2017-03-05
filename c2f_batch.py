91242b8c0d39f225cd98f1e130e0687e3dca7886
shaunakv1 <shaunakv1@gmail.com>
2017-03-05 11:54:23 -0800

    lab 1 3

--- c2f_batch.py
+++ c2f_batch.py
@@ -3,4 +3,4 @@ import sys
 sys.argv.pop(0)
 for cel in sys.argv:
 	far = (float(cel) * 1.8)+32.0
-	print "temp in far is", str(far) + u'\xb0'
+	print str(cel) +u'\xb0'+ "C is", str(far) + u'\xb0F'
---
+++ strfun.py
@@ -0,0 +1,5 @@
+name = raw_input("what is your name? ")
+print "Hello ", name
+print "Hello ", name.capitalize()
+print "Hello ", name.lower()
+print "Hello ", name.title()
