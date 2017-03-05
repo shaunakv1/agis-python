25a0ce15678e84834c199b051bc1c689df8a2017
shaunakv1 <shaunakv1@gmail.com>
2017-03-05 13:46:00 -0800

    using split list instead of pop

--- c2f_batch.py
+++ c2f_batch.py
@@ -1,6 +1,5 @@
 import sys
 
-sys.argv.pop(0)
-for cel in sys.argv:
+for cel in sys.argv[1:]:
 	far = (float(cel) * 1.8)+32.0
 	print str(cel) +u'\xb0'+ "C is", str(far) + u'\xb0F'
---
+++ conditionals.py
@@ -0,0 +1,10 @@
+value = 30
+
+if value > 50:
+    print "Wombat!"
+    print "Crikee"
+elif value > 20:
+    print "Crocodile"
+else:
+  print "Koala!"
+  
